98d761ce2d2de1e83d6db25201b6308d68bfed49
Ramkumar <spramkumar89@users.noreply.github.com>
2022-01-07 16:38:05 +0530

    Initial Commit

---
+++ spec/apm-wordcount-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ApmWordcountView from '../lib/apm-wordcount-view';
+
+describe('ApmWordcountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
